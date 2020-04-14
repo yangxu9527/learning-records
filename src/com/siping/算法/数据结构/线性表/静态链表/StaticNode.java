c140bc65afa5d2229bb7628bcc5e14479791c5e1
yangxu <xu.yang@sipingsoft.com>
2020-04-14 10:54:49 +0800

    refactor 迁移leetcode数组代码

---
+++ src/com/siping/算法/数据结构/线性表/静态链表/StaticNode.java
@@ -0,0 +1,20 @@
+package com.siping.算法.数据结构.线性表.静态链表;
+
+/**
+ * @author Yang Xu
+ * @date 2019/11/21 11:51
+ * @description: 静态链表的节点，用数组来存放链表之间的指向，第一个元素和最后的一个元素不存放数据。
+ * 第一个节点存放
+ */
+public class StaticNode {
+
+    /**
+     * 存放的数据
+     */
+    private Object data;
+    /**
+     * 下一个数据的游标
+     */
+    private int cur;
+
+}

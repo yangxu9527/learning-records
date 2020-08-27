bc4001c4d290c2d125eb97b4022a7621355e8bc3
yangxu <xu.yang@sipingsoft.com>
2020-08-27 08:48:43 +0800

    feat: 调用微软本地的文件

---
+++ src/com/siping/jna/DeleteFile.java
@@ -0,0 +1,20 @@
+package com.siping.jna;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+/**
+ * @author Yang Xu
+ * @date 2020/8/27 8:38
+ * @description: 调用微软本地的dll文件
+ */
+public interface DeleteFile extends Library {
+    DeleteFile DELETE_FILE = Native.load("Kernel32", DeleteFile.class);
+
+    /**
+     * 删除文件
+     * @param lpFileName 删除的文件名
+     * @return
+     */
+    boolean DeleteFileA(String lpFileName);
+}
--- src/com/siping/jna/JnaDemo.java
+++ src/com/siping/jna/JnaDemo.java
@@ -10,5 +10,7 @@ public class JnaDemo {
     public static void main(String[] args) {
         int value = MyDll.mydll.add(1, 2);
         System.out.println("value = " + value);
+
+        DeleteFile.DELETE_FILE.DeleteFileA("C:\\Users\\yangxu\\Desktop\\test.txt");
     }
 }
